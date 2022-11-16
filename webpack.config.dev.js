7f681e06f770bd9d4c10b4960b6fc8b4196c8516
Torofms <torofms37@gmail.com>
2022-11-15 19:17:52 -0700

    1.15

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -70,4 +70,11 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+		static: path.join(__dirname, 'dist'),
+		compress: true,
+		historyApiFallback: true,
+		port: 8080,
+		open: true,
+	}
 }
